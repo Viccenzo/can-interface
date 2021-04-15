5e2409b8f520cfe00d0a1f2e32775fd9a49aeacf
shamiro <viccenzobenetti@gmail.com>
2021-04-15 13:35:36 -0300

    Fix: Make CAN display work with menu loop system

--- main.js
+++ main.js
@@ -421,17 +421,25 @@ async function infoDisplay(){
   console.log("Starting program");
   
   setInterval(upStream,1000);
-  try{
-    await op.question('Enter an option: ', () => {
-      resolve(0);
-    })
-  }
-  catch(e){
-    resolve(0);
-  }
+  await keypress();
+  
 }
 
 // Function Definitions:
+
+// Detect a button press
+const keypress = async () => {
+  process.stdin.setRawMode(true)
+  return new Promise(resolve => process.stdin.once('data', data => {
+    const byteArray = [...data]
+    if (byteArray.length > 0 && byteArray[0] === 3) {
+      console.log('^C')
+      process.exit(1)
+    }
+    process.stdin.setRawMode(false)
+    resolve()
+  }))
+}
 
 // every 1 sec 
 function upStream(){
