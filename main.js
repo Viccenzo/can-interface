9e5fb843bd7e7a496d6b28724e3e963a17ba4b45
shamiro <viccenzobenetti@gmail.com>
2021-04-16 18:30:11 -0300

    Feat: Developping sending CAN message

--- main.js
+++ main.js
@@ -303,7 +303,7 @@ function upStream(){
   msgAvail = 0;
 }
 
-function canMsgSend(/*id,var1,var2,var3,var4*/){
+async function canMsgSend(/*id,var1,var2,var3,var4*/){
   const canSend = Buffer.from('123#11223344AABBCCDD', 'utf8');
   //msgSend.id = 0x186555F4;
   console.log(canSend)
