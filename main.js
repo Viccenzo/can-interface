957b48de12bb438595fc2597744dc977a05760dd
shamiro <viccenzobenetti@gmail.com>
2021-04-16 18:06:41 -0300

    Feat: Developping sending CAN message

--- main.js
+++ main.js
@@ -142,16 +142,16 @@ async function main() {
       console.log(response[1]);
       console.log("8");
       switch(response[1]){
-        case 8:
+        case "8":
           console.log("here1");
           //canMsgSend(response[0],response[1],[response[2],response[3],response[4],response[5],response[6],response[7],response[8],response[9]]);
-        case 16:
+        case "16":
           console.log("here2");
           //canMsgSend(response[0],response[1],[response[2],response[3],response[4],response[5]]);
-        case 32:
+        case "32":
           console.log("here3");  
           //canMsgSend(response[0],response[1],[response[2],response[3]]);
-        case 64:
+        case "64":
           console.log("here4");
           //canMsgSend(response[0],response[1],[response[2]]);
       }
