591e8d0f844769f46ef3966cf07d4561351e728a
shamiro <viccenzobenetti@gmail.com>
2021-04-16 16:35:52 -0300

    Feat: Developping sending CAN message

--- main.js
+++ main.js
@@ -307,7 +307,7 @@ function upStream(){
 async function canMsgSend(/*id,var1,var2,var3,var4*/){
   var canSendId = "123";
   var canSendMsg = "1122334455667788";
-  //console.log(`cansed can0 ${canSendId}#${canSendMsg}`);
+  console.log(`sudo cansed can0 ${canSendId}#${canSendMsg}`);
   cmd.get(`sudo cansed can0 ${canSendId}#${canSendMsg}`);
 }
 
