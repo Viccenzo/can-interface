8dbd50c40d122f98134e9e52a681ab5fcfcb98e1
shamiro <viccenzobenetti@gmail.com>
2021-04-19 12:01:32 -0300

    Feat: Developping sending CAN message

--- main.js
+++ main.js
@@ -331,8 +331,8 @@ async function canMsgSend(id,size,canInfo){
   switch(size){
     case 8:
       canMsgSend.data = Buffer.from([canInfo[0],canInfo[1],canInfo[2],canInfo[3],canInfo[4],canInfo[5],canInfo[6],canInfo[7]]);
-      console.log("here1");
-      //channel.send(canMsgSend);
+      //console.log("here1");
+      channel.send(canMsgSend);
       return 0;
     case 16:
       canMsgSend.data = Buffer.from([canInfo[0],canInfo[0]>>8,canInfo[1],canInfo[1]>>8,canInfo[2],canInfo[2]>>8,canInfo[3],canInfo[3]>>8]);
@@ -345,7 +345,8 @@ async function canMsgSend(id,size,canInfo){
       channel.send(canMsgSend);
       return 0;
     default:
-    return -1;
+      console.log("Command error!");
+      return -1;
   }
 }
 
