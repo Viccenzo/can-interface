a2e38576f4fc27a9642679c31e91320a5cc41b74
shamiro <viccenzobenetti@gmail.com>
2021-04-22 15:04:00 -0300

    Feat: Load BMS config file

--- main.js
+++ main.js
@@ -246,6 +246,7 @@ async function configRead(){
 // Falta  adequar essa funcão para mandar 16 bits apenas
 async function configSend(){
   let configFileData =  configRead();
+  console.log(configFileData);
   await canMsgSend(10000000, 16, 
     [configFileData.numberOfChannels, 
     configFileData.numberOfThermistors,
